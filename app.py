21a5c316efc786c109c81a79d392b3702f8427c6
emond alexandre <a3emond@gmail.com>
2024-10-08 09:16:37 -0400

    uses home server hosted database now

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from views.user_view import User
+from views.user_view import user
 app = Flask(__name__)
 
 app.register_blueprint(user)
 
 @app.route('/')
 def home():
-    return "Welcome to the API!"
+    return "flask runs on docker\n 1.0 - refactored"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
